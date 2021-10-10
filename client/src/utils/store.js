c514cabc04d30d3df1b41e69e4777c68ff294077
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-10 10:37:53 -0700

    adding storejs

---
+++ client/src/utils/store.js
@@ -0,0 +1,8 @@
+// Creates a Redux store that holds the state of the app. Only one store should exist.
+import { createStore } from 'redux';
+
+// Importing the reducer file that is mostly unchanged
+import reducer from './reducers';
+
+const store = createStore(reducer);
+export default store;
