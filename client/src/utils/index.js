25f7497e0a3a3c3ba00396c1729649614628db4b
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-10 08:18:39 -0700

    using dispatch in cart item

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,7 @@
+// Creates a Redux store that holds the state of the app. Only one store should exist.
 import { createStore } from 'redux';
-import reducer from './reducers';
 
-const store = createStore(reducer);
-export default store;
+// Importing the reducer file that is mostly unchanged
+import reducers from './reducers';
+
+export default createStore(reducers);
